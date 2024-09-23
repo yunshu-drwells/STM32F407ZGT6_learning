4111d5fcaee60eb3118b9099aa21d85792b2db17
yunshu-drwells <2582050678@qq.com>
2024-09-23 19:25:42 +0800

    add 24.IIC(24C02) and 38.TOUCH

---
+++ 1.arm/24.IIC(24C02)/MDK-ARM/IIC(24C02)/startup_stm32f407xx.d
@@ -0,0 +1,1 @@
+iic(24c02)\startup_stm32f407xx.o: startup_stm32f407xx.s
---
+++ 1.arm/24.IIC(24C02)/MDK-ARM/RTE/_IIC_24C02_/RTE_Components.h
@@ -0,0 +1,20 @@
+
+/*
+ * Auto generated Run-Time-Environment Component Configuration File
+ *      *** Do not modify ! ***
+ *
+ * Project: 'IIC(24C02)' 
+ * Target:  'IIC(24C02)' 
+ */
+
+#ifndef RTE_COMPONENTS_H
+#define RTE_COMPONENTS_H
+
+
+/*
+ * Define the Device Header File: 
+ */
+#define CMSIS_device_header "stm32f4xx.h"
+
+
+#endif /* RTE_COMPONENTS_H */
